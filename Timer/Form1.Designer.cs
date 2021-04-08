4335a01a3e50be952b030baeee0ecb2b43836c7f
VadimChorrny <chorrny228@gmail.com>
2021-04-08 12:16:59 +0300

    Добавьте файлы проекта.

---
+++ Timer/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace Timer_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stopButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(81, 285);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(224, 76);
+            this.stopButton.TabIndex = 0;
+            this.stopButton.Text = "STOP";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(496, 285);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(224, 76);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(280, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 57);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Timer Yopta";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.stopButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Timer/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer_
+{
+    public partial class Form1 : Form
+    {
+        public DateTime data = new DateTime();
+        Timer timer = new Timer();
+        public Stopwatch sw = new Stopwatch();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            long tick = DateTime.Now.Ticks - data.Ticks;
+            DateTime stopWatch = new DateTime();
+            stopWatch = stopWatch.AddTicks(tick);
+            label1.Text = String.Format("{0:HH:mm:ss:ff}", stopWatch);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            data = DateTime.Now;
+            timer.Stop();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            data = DateTime.Now;
+            timer.Interval = 10;
+            timer.Tick += new EventHandler(label1_Click);
+            timer.Start();
+        }
+    }
+}
